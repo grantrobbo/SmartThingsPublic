af99ea977a1589275cc80eba1ae7e96b9c1039ac
Grant Roberts <grantroberts@live.com.au>
2018-01-19 17:05:31 +1000

    Update 19 Jan 2018

---
+++ devicetypes/bspranger/xiaomi-aqara-motion-sensor.src/xiaomi-aqara-motion-sensor.groovy
@@ -0,0 +1,332 @@
+/**
+ *  Xiaomi Aqara Motion Sensor
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Based on original DH by Eric Maycock 2015
+ *
+ *  Change log:
+ *  modified 29/12/2016 a4refillpad
+ *  Added fingerprinting
+ *  Added heartbeat/lastcheckin for monitoring
+ *  Added battery and refresh
+ *  Motion background colours consistent with latest DH
+ *  Fixed max battery percentage to be 100%
+ *  Added Last update to main tile
+ *  Added last motion tile
+ *  Heartdeat icon plus improved localisation of date
+ *  Removed non working tiles and changed layout and incorporated latest colours
+ *  Added experimental health check as worked out by rolled54.Why
+ *  bspranger - renamed to bspranger to remove confusion of a4refillpad
+ */
+
+metadata {
+    definition (name: "Xiaomi Aqara Motion Sensor", namespace: "bspranger", author: "bspranger") {
+        capability "Motion Sensor"
+        capability "Illuminance Measurement"
+        capability "Configuration"
+        capability "Battery"
+        capability "Sensor"
+        capability "Refresh"
+        capability "Health Check"
+
+        attribute "lastCheckin", "String"
+        attribute "lastMotion", "String"
+        attribute "light", "number"
+        attribute "batteryRuntime", "String"
+
+        fingerprint profileId: "0104", deviceId: "0104", inClusters: "0000, 0003, FFFF, 0019", outClusters: "0000, 0004, 0003, 0006, 0008, 0005, 0019", manufacturer: "LUMI", model: "lumi.sensor_motion", deviceJoinName: "Xiaomi Motion"
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "0107", inClusters: "0000,FFFF,0406,0400,0500,0001,0003", outClusters: "0000,0019", manufacturer: "LUMI", model: "lumi.sensor_motion.aq2", deviceJoinName: "Xiaomi Aqara Motion Sensor"
+
+        command "resetBatteryRuntime"
+        command "reset"
+        command "Refresh"
+    }
+
+    simulator {
+    }
+
+    preferences {
+        input "motionReset", "number", title: "Number of seconds after the last reported activity to report that motion is inactive (in seconds). \n\n(The device will always remain blind to motion for 60seconds following first detected motion. This value just clears the 'active' status after the number of seconds you set here but the device will still remain blind for 60seconds in normal operation.)", description: "", value:120, displayDuringSetup: true
+    }
+
+    tiles(scale: 2) {
+        multiAttributeTile(name:"motion", type: "generic", width: 6, height: 4) {
+            tileAttribute ("device.motion", key: "PRIMARY_CONTROL") {
+                attributeState "active", label:'motion', icon:"st.motion.motion.active", backgroundColor:"#00a0dc"
+                attributeState "inactive", label:'no motion', icon:"st.motion.motion.inactive", backgroundColor:"#ffffff"
+            }
+            tileAttribute("device.lastMotion", key: "SECONDARY_CONTROL") {
+                attributeState("default", label:'Last Motion: ${currentValue}')
+            }
+        }
+        valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+            state "default", label:'${currentValue}%', unit:"",
+            backgroundColors: [
+                [value: 10, color: "#bc2323"],
+                [value: 26, color: "#f1d801"],
+                [value: 51, color: "#44b621"]
+            ]
+        }
+        valueTile("light", "device.Light", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
+		state "light", label:'${currentValue} ${unit}', unit:"lux"
+        }
+        standardTile("reset", "device.reset", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action:"reset", label: "Reset Motion", icon:"st.motion.motion.active"
+        }
+        valueTile("lastcheckin", "device.lastCheckin", decoration: "flat", inactiveLabel: false, width: 4, height: 1) {
+            state "default", label:'Last Checkin:\n ${currentValue}'
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
+        standardTile("refresh", "command.refresh", inactiveLabel: false) {
+            state "default", label:'refresh', action:"refresh.refresh", icon:"st.secondary.refresh-icon"
+        }
+		valueTile("batteryRuntime", "device.batteryRuntime", inactiveLabel: false, decoration: "flat", width: 4, height: 1) {
+			state "batteryRuntime", label:'Battery Changed (tap to reset):\n ${currentValue}', action:"resetBatteryRuntime"
+		}
+        standardTile("empty1x1", "null", width: 1, height: 1, decoration: "flat") {
+			state "emptySmall", label:'', defaultState: true
+		}
+
+        main(["motion"])
+        details(["motion", "battery", "light", "reset", "lastcheckin", "refresh", "batteryRuntime"])
+    }
+}
+
+def parse(String description) {
+    log.debug "${device.displayName} Parsing: $description"
+    
+    // send event for heartbeat
+    def now = new Date().format("EEE MMM dd yyyy h:mm:ss a", location.timeZone)
+    sendEvent(name: "lastCheckin", value: now)
+
+    Map map = [:]
+    if (description?.startsWith('catchall:')) {
+        map = parseCatchAllMessage(description)
+    }
+    else if (description?.startsWith('read attr -')) {
+        map = parseReportAttributeMessage(description)
+    }
+    else if (description?.startsWith('illuminance:')) {
+        map = parseIlluminanceMessage(description)
+    }
+    else if (description?.startsWith('enroll request')) {
+        List cmds = enrollResponse()
+        log.debug "${device.displayName} enroll response: ${cmds}"
+        result = cmds?.collect { new physicalgraph.device.HubAction(it) }
+    }
+
+    log.debug "${device.displayName} Parse returned: $map"
+    def result = map ? createEvent(map) : null
+    
+    return result
+}
+
+private Map parseIlluminanceMessage(String description) {
+
+    def Lux = ((description - "illuminance: ").trim()) as Float
+
+    def result = [
+        name: 'Light',
+        value: Lux,
+        unit: "Lux",
+        isStateChange:true,
+        descriptionText : "${device.displayName} Light was ${Lux} Lux"
+    ]
+    return result;
+}
+
+
+private Map getBatteryResult(rawValue) {
+    def rawVolts = rawValue / 1000
+
+    def minVolts = 2.7
+    def maxVolts = 3.3
+    def pct = (rawVolts - minVolts) / (maxVolts - minVolts)
+    def roundedPct = Math.min(100, Math.round(pct * 100))
+
+    def result = [
+        name: 'battery',
+        value: roundedPct,
+        unit: "%",
+        isStateChange:true,
+        descriptionText : "${device.displayName} raw battery is ${rawVolts}v"
+    ]
+    
+    log.debug "${device.displayName}: ${result}"
+    if (state.battery != result.value)
+    {
+    	state.battery = result.value
+        resetBatteryRuntime()
+    }
+    return result
+}
+
+private Map parseCatchAllMessage(String description) {
+    def i
+    Map resultMap = [:]
+    def cluster = zigbee.parse(description)
+    log.debug cluster
+    if (cluster) {
+        switch(cluster.clusterId) 
+        {
+            case 0x0000:
+                def MsgLength = cluster.data.size();
+
+                // Original Xiaomi CatchAll does not have identifiers, first UINT16 is Battery
+                if ((cluster.data.get(0) == 0x02) && (cluster.data.get(1) == 0xFF))
+                {
+                    for (i = 0; i < (MsgLength-3); i++)
+                    {
+                        if (cluster.data.get(i) == 0x21) // check the data ID and data type
+                        {
+                            // next two bytes are the battery voltage.
+                            resultMap = getBatteryResult((cluster.data.get(i+2)<<8) + cluster.data.get(i+1))
+                            break
+                        }
+                    }
+                }else if ((cluster.data.get(0) == 0x01) && (cluster.data.get(1) == 0xFF))
+                {
+                    for (i = 0; i < (MsgLength-3); i++)
+                    {
+                        if ((cluster.data.get(i) == 0x01) && (cluster.data.get(i+1) == 0x21))  // check the data ID and data type
+                        {
+                            // next two bytes are the battery voltage.
+                            resultMap = getBatteryResult((cluster.data.get(i+3)<<8) + cluster.data.get(i+2))
+                            break
+                        }
+                    }
+                }
+            break
+        }
+    }
+    return resultMap
+}
+
+
+def configure() {
+	state.battery = 0
+    log.debug "${device.displayName}: configuring"
+    return zigbee.readAttribute(0x0001, 0x0020) + zigbee.configureReporting(0x0001, 0x0020, 0x21, 600, 21600, 0x01)
+}
+
+def refresh(){
+    log.debug "${device.displayName}: refreshing"
+    return zigbee.readAttribute(0x0001, 0x0020) + zigbee.configureReporting(0x0001, 0x0020, 0x21, 600, 21600, 0x01)
+}
+
+def enrollResponse() {
+    log.debug "${device.displayName}: Enrolling device into the IAS Zone"
+    [
+        // Enrolling device into the IAS Zone
+        "raw 0x500 {01 23 00 00 00}", "delay 200",
+        "send 0x${device.deviceNetworkId} 1 1"
+    ]
+}
+
+private Map parseReportAttributeMessage(String description) {
+    def cluster = description.split(",").find {it.split(":")[0].trim() == "cluster"}?.split(":")[1].trim()
+    def attrId = description.split(",").find {it.split(":")[0].trim() == "attrId"}?.split(":")[1].trim()
+    def value = description.split(",").find {it.split(":")[0].trim() == "value"}?.split(":")[1].trim()
+
+    Map resultMap = [:]
+    def now = new Date().format("EEE MMM dd yyyy h:mm:ss a", location.timeZone)
+
+    if ((cluster == "0406") && (attrId == "0000"))
+    {
+        def motion = (value == "01") ? "active" : "inactive"
+        sendEvent(name: "lastMotion", value: now)
+        if (settings.motionReset == null || settings.motionReset == "" ) settings.motionReset = 120
+        if (motion == "active") runIn(settings.motionReset, stopMotion)
+        resultMap = getMotionResult(motion)
+    }
+    else if (cluster == "0000" && attrId == "0005")  
+    {
+        def modelName = ""
+        // Parsing the model
+        for (int i = 0; i < value.length(); i+=2) 
+        {
+            def str = value.substring(i, i+2);
+            def NextChar = (char)Integer.parseInt(str, 16);
+            modelName = modelName + NextChar
+        }
+        log.debug "${device.displayName} reported: cluster: ${cluster}, attrId: ${attrId}, value: ${value}, model:${modelName}"
+    }
+    return resultMap
+}
+
+
+private Map parseIasMessage(String description) {
+    List parsedMsg = description.split(' ')
+    String msgCode = parsedMsg[2]
+
+    Map resultMap = [:]
+    switch(msgCode) {
+        case '0x0020': // Closed/No Motion/Dry
+            resultMap = getMotionResult('inactive')
+            break
+
+        case '0x0021': // Open/Motion/Wet
+            resultMap = getMotionResult('active')
+            break
+
+        case '0x0022': // Tamper Alarm
+            log.debug '${device.displayName}: motion with tamper alarm'
+            resultMap = getMotionResult('active')
+            break
+
+        case '0x0023': // Battery Alarm
+            break
+
+        case '0x0024': // Supervision Report
+            log.debug '${device.displayName}: no motion with tamper alarm'
+            resultMap = getMotionResult('inactive')
+            break
+
+        case '0x0025': // Restore Report
+            break
+
+        case '0x0026': // Trouble/Failure
+            log.debug '${device.displayName}: motion with failure alarm'
+            resultMap = getMotionResult('active')
+            break
+
+        case '0x0028': // Test Mode
+            break
+    }
+    return resultMap
+}
+
+
+private Map getMotionResult(value) {
+    String descriptionText = value == 'active' ? "${device.displayName} detected motion" : "${device.displayName} motion has stopped"
+    def commands = [
+        name: 'motion',
+        value: value,
+        descriptionText: descriptionText
+    ]
+    return commands
+}
+
+def stopMotion() {
+    sendEvent(name:"motion", value:"inactive")
+}
+
+def reset() {
+    sendEvent(name:"motion", value:"inactive")
+}
+
+def resetBatteryRuntime() {
+    def now = new Date().format("MMM dd yyyy", location.timeZone)
+    sendEvent(name: "batteryRuntime", value: now)
+}
